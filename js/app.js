b737fe5c048e86d4853438b4a2b3dd634b41f8b5
Ben Grimes <ben.grimes@pardot.com>
2013-06-30 12:27:40 -0500

    Adding file list

--- js/app.js
+++ js/app.js
@@ -3,3 +3,9 @@ var App = Ember.Application.create();
 App.Router.map(function(){
   this.resource('files');
 });
+
+App.FilesRoute = Ember.Route.extend({
+  model: function() {
+    return FILES;
+  }
+});
