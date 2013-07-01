7c4d7a2daf58a5862ec4b7708ddeb7592bcc4382
Ben Grimes <ben.grimes@pardot.com>
2013-07-01 10:05:31 -0500

    Move file to nested resource

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 var App = Ember.Application.create();
 
 App.Router.map(function(){
-  this.resource('files');
-  this.resource('file', {path: '/file/:file_id'});
+  this.resource('files', function(){
+      this.resource('file', {path: ':file_id'});
+  });
 });
 
 App.FilesRoute = Ember.Route.extend({
